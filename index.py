c24b25480c856b4c9f81db2795b263698dc71c62
TRuhm <ruhm.tristan@web.de>
2023-11-16 16:49:38 +0100

    current prompts

--- index.py
+++ index.py
@@ -1,2 +1,42 @@
+#!C:\Users\ruhmt\AppData\Local\Programs\Python\Python311\python.exe
 import streamlit as st
 
+print("Content-type: text/html")
+print()
+
+
+
+with st.sidebar:
+    st.title('LLM-TTTM')
+
+
+#with st.form(key='search_form'):
+#    st.text_input("Search patents", key="phrases")
+#    st.form_submit_button(label="Search") 
+
+import streamlit as st
+
+st.title("Patent Pete")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
